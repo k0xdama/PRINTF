96497e43efa3c7f8139ed2f7693935085b050d1e
Mateo PRIOUX <pmateo@paul-f4Br6s1.clusters.42paris.fr>
2023-08-16 21:32:23 +0200

    some changes

--- SRCS/ft_hexa.c
+++ SRCS/ft_hexa.c
@@ -6,7 +6,33 @@
 /*   By: pmateo <pmateo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 22:44:43 by pmateo            #+#    #+#             */
-/*   Updated: 2023/08/14 22:44:44 by pmateo           ###   ########.fr       */
+/*   Updated: 2023/08/16 21:02:55 by pmateo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <unistd.h>
+
+int ft_hexalen(unsigned int nb)
+{
+    int    len;
+
+    len = 0;
+    while (nb != 0)
+    {
+        nb /= 16;
+        len++;
+    }
+    return (len);
+}
+
+int ft_puthexa(unsigned int, const char flag)
+{
+    
+}
+
+int main(void)
+{
+    unsigned int nb = 420;
+    printf("%x\n", nb);
+}
